0728679bcefda763c0de6039fff4b16407ea6637
Oleg Afanasev <OAAfanasev@inno.tech>
2024-12-04 23:53:23 +0300

    layout & profile

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -9,6 +9,8 @@ import { RootPage, NoFound, ErrorBoundary } from "@/pages/root";
 import { PageLoader } from "@/shared/ui";
 import { initI18n } from "@/entities/i18n";
 
+import { AppLayout } from "../ui/Layout";
+
 import type { Client } from "urql";
 
 export type RouterContext = {
@@ -16,24 +18,28 @@ export type RouterContext = {
   client: Client;
 };
 
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+
 const rootRoute = createRootRouteWithContext<RouterContext>()({
   component: RootPage,
-  beforeLoad: ({ context }) => {
-    const userLogin = localStorage.getItem("login");
-
-    updateAbility(context.ability, userLogin);
-
+  loader: () => {
     return Promise.all([initI18n()]);
   },
+  staleTime: MINUTE * 10,
+  gcTime: MINUTE * 10,
   notFoundComponent: NoFound,
   pendingComponent: PageLoader,
   errorComponent: ErrorBoundary,
 });
 
-const indexRoute = createRoute({
-  path: "/",
+const authenticatedRoute = createRoute({
+  id: "authenticated",
   getParentRoute: () => rootRoute,
   beforeLoad: ({ location, context }) => {
+    const userLogin = localStorage.getItem("login");
+    updateAbility(context.ability, userLogin);
+
     if (context.ability.cannot("read", "all")) {
       throw redirect({
         to: "/login",
@@ -44,10 +50,23 @@ const indexRoute = createRoute({
       });
     }
   },
+  component: AppLayout,
+});
+
+const indexRoute = createRoute({
+  path: "/",
+  getParentRoute: () => authenticatedRoute,
   loader: (options) =>
     import("@/pages/home/api/loader").then((d) => d.loader(options.context)),
   pendingComponent: PageLoader,
 }).lazy(() => import("@/pages/home").then((d) => d.Route));
+
+const profileRoute = createRoute({
+  path: "profile",
+  getParentRoute: () => authenticatedRoute,
+  loader: () => import("@/pages/profile/api/loader").then((d) => d.loader()),
+  pendingComponent: PageLoader,
+}).lazy(() => import("@/pages/profile").then((d) => d.Route));
 
 const loginRoute = createRoute({
   path: "/login",
@@ -72,4 +91,23 @@ const loginRoute = createRoute({
   pendingComponent: PageLoader,
 }).lazy(() => import("@/pages/login").then((d) => d.Route));
 
-export const routeTree = rootRoute.addChildren([loginRoute, indexRoute]);
+const logoutRoute = createRoute({
+  path: "/logout",
+  getParentRoute: () => rootRoute,
+  beforeLoad: ({ context }) => {
+    console.log("logoutRoute.beforeLoad");
+    updateAbility(context.ability, null);
+    localStorage.removeItem("login");
+    throw redirect({
+      to: "/",
+      replace: true,
+    });
+  },
+  pendingComponent: PageLoader,
+});
+
+export const routeTree = rootRoute.addChildren([
+  authenticatedRoute.addChildren([profileRoute, indexRoute]),
+  loginRoute,
+  logoutRoute,
+]);
