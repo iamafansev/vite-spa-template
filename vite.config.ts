95b23bbb658884149ff357c35ec957b47e4b57ca
Oleg Afanasev <OAAfanasev@inno.tech>
2025-06-04 22:03:14 +0700

    remove gql

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import webfontDownload from "vite-plugin-webfont-dl";
 import svgr from "vite-plugin-svgr";
 import { VitePWA } from "vite-plugin-pwa";
-import codegen from "vite-plugin-graphql-codegen";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 import manifest from "./manifest.json";
-import { getCodegenConfig } from "./codegen";
 
 // vitest automatically sets NODE_ENV to 'test' when running tests
 const isTest = process.env.NODE_ENV === "test";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const graphqlSchema = env.VITE_API_URL;
-
+export default defineConfig(() => {
   return {
     plugins: [
       !isTest && TanStackRouterVite(),
@@ -25,14 +20,6 @@ export default defineConfig(({ mode }) => {
       tsconfigPaths(),
       webfontDownload(),
       svgr(),
-      codegen({
-        runOnStart: false,
-        runOnBuild: false,
-        configOverrideWatcher: {
-          watch: ["src/**/*.{ts,tsx}", "!src/gql/**/*"],
-        },
-        config: getCodegenConfig({ schema: graphqlSchema }),
-      }),
       VitePWA({
         manifest,
         includeAssets: [
